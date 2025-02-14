7f4202a320744a536e398f6206a4f0ea8af068c4
Jessica Lu <jgujialu@gmail.com>
2025-02-13 22:13:19 -0500

    set default loggedIn = false

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react'
 export const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
-    const [loggedIn, setLoggedIn] = useState();
+    const [loggedIn, setLoggedIn] = useState(false);
 
     return (
         <AuthContext value={{ loggedIn, setLoggedIn }}>
