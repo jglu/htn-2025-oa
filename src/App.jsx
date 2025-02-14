c2c4366b69e032e22074dfa482d14c4e780835f8
Jessica Lu <jgujialu@gmail.com>
2025-02-13 21:51:49 -0500

    add auth context (TODO: integrate in Events.jsx and Login.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { EventsProvider } from "./context/EventsContext"
+import { AuthProvider } from "./context/AuthContext"
 
 import Events from "./pages/Events"
 import Login from "./pages/Login"
@@ -8,17 +9,19 @@ import NotFound from "./pages/NotFound"
 function App() {
 
   return (
-    <EventsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Events />} />
-          <Route path="events" element={<Navigate to="/" replace />} />
-          <Route path="login" element={<Login />} />
-          <Route path="/404" element={<NotFound />} />
-          <Route path="*" element={<Navigate to="/404" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </EventsProvider>
+    <AuthProvider>
+      <EventsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Events />} />
+            <Route path="events" element={<Navigate to="/" replace />} />
+            <Route path="login" element={<Login />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/404" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </EventsProvider>
+    </AuthProvider>
   )
 }
 
