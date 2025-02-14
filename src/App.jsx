4b5db2a9850e784367c8474036e76b72d753f29b
Jessica Lu <jgujialu@gmail.com>
2025-02-13 23:10:45 -0500

    styling: center, import fonts

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,26 @@ import Events from "./pages/Events"
 import Login from "./pages/Login"
 import NotFound from "./pages/NotFound"
 
+import './app.css'
+
 function App() {
 
   return (
-    <AuthProvider>
-      <EventsProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Events />} />
-            <Route path="events" element={<Navigate to="/" replace />} />
-            <Route path="login" element={<Login />} />
-            <Route path="/404" element={<NotFound />} />
-            <Route path="*" element={<Navigate to="/404" replace />} />
-          </Routes>
-        </BrowserRouter>
-      </EventsProvider>
-    </AuthProvider>
+    <div className="app">
+      <AuthProvider>
+        <EventsProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Events />} />
+              <Route path="events" element={<Navigate to="/" replace />} />
+              <Route path="login" element={<Login />} />
+              <Route path="/404" element={<NotFound />} />
+              <Route path="*" element={<Navigate to="/404" replace />} />
+            </Routes>
+          </BrowserRouter>
+        </EventsProvider>
+      </AuthProvider>
+    </div>
   )
 }
 
